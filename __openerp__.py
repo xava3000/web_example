f400d37a267024aebe55136326ad77238560de4a
Xavier Valencia (Oh! Digital) <xava3000@gmail.com>
2014-07-24 19:42:31 +0200

    [INIT]Sample Web module. Initial declarations

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# __openerp__.py
+{
+    'name': "Web Example",
+    'description': "Basic example of a (future) web module",
+    'category': 'Hidden',
+    'depends': ['web'],
+    'js': ['static/src/js/first_module.js'],
+}
+
---
+++ static/src/js/first_module.js
@@ -0,0 +1,4 @@
+
+// static/src/js/first_module.js
+console.log("Debug statement: file loaded");
+
